d12fb376c986c4287babfe138940b6e3098e425b
Zoran <zmandic@sait.ca>
2016-05-04 20:21:22 -0600

    Added Model

---
+++ Buzzer/Answer.swift
@@ -0,0 +1,9 @@
+//
+//  Answer.swift
+//  Buzzer
+//
+//  Created by Zoran Mandic on 2016-05-04.
+//  Copyright © 2016 Zoran Mandic. All rights reserved.
+//
+
+import Foundation
---
+++ Buzzer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Buzzer
+//
+//  Created by Zoran Mandic on 2016-05-04.
+//  Copyright © 2016 Zoran Mandic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

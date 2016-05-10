a100b8173d34790aeb7806e09f944b35f84bd226
zoran mandic <zoran.mandic@edu.sait.ca>
2016-05-09 20:59:07 -0600

    gjgjgh

---
+++ Buzzer/PlayerInputViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PlayerInputViewController.swift
+//  Buzzer
+//
+//  Created by Zoran Mandic on 2016-05-09.
+//  Copyright © 2016 Zoran Mandic. All rights reserved.
+//
+
+import UIKit
+
+class PlayerInputViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Buzzer/QuestionPickerViewController.swift
@@ -0,0 +1,38 @@
+//
+//  QuestionPickerViewController.swift
+//  Buzzer
+//
+//  Created by Zoran Mandic on 2016-05-09.
+//  Copyright © 2016 Zoran Mandic. All rights reserved.
+//
+
+import UIKit
+
+class QuestionPickerViewController: UIViewController {
+    
+    @IBOutlet weak var questionCollectionView:
+    UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
